82ed9cbd44afa36d7c74ad118a7c4f6cc106b5d9
niulele <15735104217@163.com>
2021-09-11 20:20:21 +0800

    add tmx

---
+++ WechatIMG13.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="WechatIMG13" tilewidth="32" tileheight="32" tilecount="962" columns="74">
+ <image source="WechatIMG13.png" width="2390" height="440"/>
+</tileset>
