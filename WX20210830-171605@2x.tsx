2ac436731f20ec04935a88ffffba2edf3d37b2da
niulele <15735104217@163.com>
2021-09-14 12:45:07 +0800

    remove useless file

--- WX20210830-171605@2x.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="WX20210830-171605@2x" tilewidth="32" tileheight="32" tilecount="702" columns="26">
- <image source="Downloads/WX20210830-171605@2x.png" width="846" height="864"/>
-</tileset>
